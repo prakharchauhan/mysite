0962acb12bebca4201b7a8572b02cde490253270
prakhar chauhan <prakharchauhan@prakhars-Air.bbrouter>
2025-03-17 18:09:08 +0530

    Prelim app

---
+++ polls/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView
+from . import views
+app_name = "polls"
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    #path('login/', LoginView.as_view(template_name="login.html"), name='login'),
+    path('login/', views.custom_login, name='login'),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("login/", views.custom_login, name="login"),
+    path("logout/", views.custom_logout, name="logout"),
+    path("register/", views.register, name="register"),
+]
---
+++ polls/views.py
@@ -0,0 +1,87 @@
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login,logout
+from django.views import generic
+from django.utils import timezone
+from django.contrib import messages
+from .models import Choice, Question
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+def custom_login(request):
+    #import pdb;pdb.set_trace()
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            next_url = request.GET.get('next', reverse("polls:index"))
+            return HttpResponseRedirect(next_url)       
+        else:
+            messages.error(request, 'Invalid username or password')
+    
+    return render(request, "polls/login.html")
+
+def custom_logout(request):
+    
+    logout(request)
+    return redirect("polls:login")
+@login_required
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect("polls:index")
+        else:
+            messages.error(request, "Registration failed. Please check the details.")
+    else:
+        form = UserCreationForm()
+    
+    return render(request, "polls/register.html", {"form": form})
